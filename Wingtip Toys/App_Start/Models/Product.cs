b291f11ba3c26ba4bfb1462dd142f4baf1578896
Pashupatibhattarai <itspashupati@yahoo.com>
2017-04-19 11:11:32 -0600

    Add project files.

---
+++ Wingtip Toys/App_Start/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Wingtip_Toys.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+        public double? UnitPrice { get; set; }
+        public int? CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+
+}
